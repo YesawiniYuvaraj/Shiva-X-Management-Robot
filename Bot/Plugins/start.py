ce3e16374b025caa001f0b9fac448c912f13acfa
Sanjeev <116713311+YesawiniYuvaraj@users.noreply.github.com>
2024-05-06 22:25:29 +0530

    Update start.py

--- Bot/Plugins/start.py
+++ Bot/Plugins/start.py
@@ -1,1 +1,22 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from Bot import app 
 
+@app.on_message(filters.command("start") & filters.private)
+async def start(client, message):
+    await message.reply_text("⚡️")
+    await message.edit_text("⚡️")
+    await message.edit_text("✋")
+
+    keyboard = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("Command", callback_data="com")]]
+    )
+
+    await message.reply_text(
+        f"Hey {message.from_user.first_name}, 🥀\n"
+        "๏ ᴛʜɪs ɪs Shiva, !\n"
+        "➻ Shiva ɪs ᴀ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ʙᴏᴛ ᴡʜɪᴄʜ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴍᴀɴᴀɢᴇ ᴀɴᴅ sᴇᴄᴜʀᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ʜᴜɢᴇ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ᴘʟᴜɢɪɴs.\n"
+        "──────────────────\n"
+        "๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʜᴇʟᴩ ʙᴜᴛᴛᴏɴ ᴛᴏ ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴍʏ ᴍᴏᴅᴜʟᴇs ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅs.",
+        reply_markup=keyboard
+  )
