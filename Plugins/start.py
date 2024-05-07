74284c1d1e0d346ae0eed2aa2a1d9a22089b6ea5
Sanjeev <116713311+YesawiniYuvaraj@users.noreply.github.com>
2024-05-07 21:15:35 +0530

    Update start.py

--- Plugins/start.py
+++ Plugins/start.py
@@ -10,11 +10,12 @@ async def start(client, message):
         [[InlineKeyboardButton("Command", callback_data="com")]]
     )
 
-    await message.reply_text(
-        f"Hey {message.from_user.first_name}, 🥀\n"
-        "๏ ᴛʜɪs ɪs Shiva, !\n"
-        "➻ Shiva ɪs ᴀ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ʙᴏᴛ ᴡʜɪᴄʜ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴍᴀɴᴀɢᴇ ᴀɴᴅ sᴇᴄᴜʀᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ʜᴜɢᴇ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ᴘʟᴜɢɪɴs.\n"
-        "──────────────────\n"
-        "๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʜᴇʟᴩ ʙᴜᴛᴛᴏɴ ᴛᴏ ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴍʏ ᴍᴏᴅᴜʟᴇs ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅs.",
+    await message.reply_photo(
+        photo="https://telegra.ph/file/40b478d7e9c0a7df55881.jpg",
+        caption=f"Hey {message.from_user.first_name}, 🥀\n"
+                "๏ ᴛʜɪs ɪs Shiva, !\n"
+                "➻ Shiva ɪs ᴀ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ʙᴏᴛ ᴡʜɪᴄʜ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴍᴀɴᴀɢᴇ ᴀɴᴅ sᴇᴄᴜʀᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ʜᴜɢᴇ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ᴘʟᴜɢɪɴs.\n"
+                "──────────────────\n"
+                "๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʜᴇʟᴩ ʙᴜᴛᴛᴏɴ ᴛᴏ ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴍʏ ᴍᴏᴅᴜʟᴇs ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅs.",
         reply_markup=keyboard
     )
