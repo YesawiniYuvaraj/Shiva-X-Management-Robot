fda12f06511acda5514d568c1cb596b6290b16ef
Sanjeev <116713311+YesawiniYuvaraj@users.noreply.github.com>
2024-05-07 20:59:27 +0530

    Update start.py

--- Plugins/start.py
+++ Plugins/start.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from Bot import app 
 
 @app.on_message(filters.command("start") & filters.private)
-async def start(client, query):
+async def start(client, message, query):
     await query.message.reply_text("⚡️")
     await query.message.edit_text("⚡️")
     await query.message.edit_text("✋")
