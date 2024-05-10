6712b16a18400fc747b4ceec3b891b86e423bab2
Sanjeev <116713311+YesawiniYuvaraj@users.noreply.github.com>
2024-05-10 22:43:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 api_id = "18990697"
 api_hash = "f4815b9a16cb03c2f5eabe8db1cb0903"
-bot_token = "7186999033:AAEQDSX9-WRXfcGGnRedUgm1_sLywldmDN8"
+bot_token = "6574755955:AAFHmpR7d04OCgpdmu0PfWHdmfpHN2nDk3s"
